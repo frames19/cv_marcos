ef680ac990692bfad16b1ebca8ae7d14c5c1d6d8
frames <marcostimanaramos@gmail.com>
2025-10-23 20:37:55 -0500

    Fix routing for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/cv_marcos/", // 👈 CAMBIO AQUÍ
   server: {
     host: "::",
     port: 8080,
