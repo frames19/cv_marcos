ae2fd8fb9900beba6442b875d301d431d5793dcd
frames <marcostimanaramos@gmail.com>
2025-10-23 20:18:43 -0500

    Versión inicial de mi CV

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/timana-ai-code/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
